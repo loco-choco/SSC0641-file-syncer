2144fc8817f2b44c26fe2d232912cd17d93c9120
Locochoco <ivanrwpf@gmail.com>
2024-10-05 23:44:40 -0300

    added nix flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
-  description = "A very basic flake";
+  description = "SSC0641 file-syncer";
 
   inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.file-syncer =
-      nixpkgs.x86_64-linux.stdenv.mkDerivation {
-        name = "libfoo-1.2.3";
+    packages.x86_64-linux.default =
+      nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
+        name = "file-syncer";
         src = ./.;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp out/daemon $out/bin
+          cp out/client $out/bin
+        '';
       };
   };
 }
